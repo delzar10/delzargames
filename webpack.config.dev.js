4aaab97c46600513aa13bd44f9793062293b1833
Mike <zarate302_5@hotmail.com>
2017-03-07 20:52:05 -0600

    bootstrap implementation

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -20,11 +20,14 @@ export default {
    )], //catching error, hot reloading, linting
   module: { // file types wanna to handle
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract("css?sourceMap")}, //!autoprefixer-loader
-      {test: /\.scss$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract("css?sourceMap?!sass?sourceMap")},
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader:"url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file" }
+      {test: /\.js$/, loaders: ['babel']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract("css?sourceMap")}, //!autoprefixer-loader
+      {test: /\.scss$/, loader: ExtractTextPlugin.extract("css?sourceMap?!sass?sourceMap")},
+      {test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000' },
+      {test: /\.(ttf|eot)$/, loader: 'file-loader' },
+      {test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports-loader?jQuery=jquery' }
+     // {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader:"url?limit=10000&mimetype=application/font-woff" },
+     // {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file" },
     ]
   }
 }
